3ff0b0a80d3c04c8fdefc03912178e4f8ff46b40
julio4110-desk <julio4110@hotmail.com>
2020-04-29 14:08:31 -0300

    Category

---
+++ src/main/java/com/example/springintro/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.example.springintro.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.springintro.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
